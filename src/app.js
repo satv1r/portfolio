a75502a322e3d2394e460a8e8e04718648a4cff6
Satvir Sandhu <356Sandhu@gmail.com>
2020-07-23 18:34:34 -0400

    changed header and about

--- src/app.js
+++ src/app.js
@@ -1,1 +1,15 @@
-// feather.replace();
+let indic = document.getElementById("scroll");
+const scrollPos = 100;
+
+const scrolly = () => {
+  let windowY = window.scrollY;
+  console.log(windowY);
+  console.log(scrollPos);
+  if (windowY > scrollPos) {
+    indic.style.display = "none";
+  } else {
+    indic.style.display = "flex";
+  }
+};
+
+window.addEventListener("scroll", scrolly);
